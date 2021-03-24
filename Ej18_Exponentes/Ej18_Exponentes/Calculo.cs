744e4c830c8e617d34244fdb20722e6fcc35aa72
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-23 21:16:20 -0600

    Add files via upload

---
+++ Ej18_Exponentes/Ej18_Exponentes/Calculo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej18_Exponentes
+{
+    class Calculo
+    {
+        public double Exponente (double x)
+        {
+            
+            double suma = 0;
+            for (double i = 1; i <= x; i++)
+            {
+                double a = 1;
+                double b = Math.Pow(x, i);
+                for (int j = 1; j <= i; j++)
+                {
+                    a = a * j;
+
+                }
+                double d = b / a;
+                suma = suma + d;
+
+            }
+            suma = suma + 1;
+            return suma;
+        }
+    }
+}
